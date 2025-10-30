faa8239d2a6800a00dbb0a108ec06d24f53e69eb
santhiya08shanmugam <santhiya08shanmugam@gmail.com>
2025-10-30 15:43:04 -0700

    Fix: add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/React/', // 👈 Replace "React" with your GitHub repo name (case-sensitive)
+  base: '/React/', // 👈 must match your GitHub repo name (case-sensitive)
 })
-
